8be7e1f73ee72e5bc29fd6dda586b97f9c112b5c
Sunjin Lee <styner32@gmail.com>
2018-05-15 23:32:37 -0700

    create db when it does not exist. Change port

--- run_service.sh
+++ run_service.sh
@@ -11,12 +11,17 @@ fi
 if [ -e requirements-development.txt ]; then
     env/bin/pip -q install -r requirements-development.txt
 fi
-if [ -e requirements-testing.txt ]; then
-    env/bin/pip -q install -r requirements-testing.txt
+
+DBNAME=toshiid_dev
+if [[ $(psql -d postgres -c "SELECT datname from pg_database WHERE datname='$DBNAME'" | grep $DBNAME) ]]; then
+  echo "$DBNAME exists"
+else
+  echo "$DBNAME does not exists"
+  createdb $DBNAME
 fi
 
 export DATABASE_URL=postgresql://sunjinlee:@localhost:5432/toshiid_dev
 export REDIS_URL=redis://127.0.0.1:6379
 export PGSQL_SSL_DISABLED=1
 
-env/bin/python -m toshiid --port=8080
+env/bin/python -m toshiid --port=3200
